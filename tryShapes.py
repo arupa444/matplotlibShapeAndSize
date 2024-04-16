d2c8de33b405c78fa9c082b2eeea7e2bd9a825f4
Arupa Nanda Swain <140882921+arupa444@users.noreply.github.com>
2024-04-16 23:00:21 +0530

    Update tryShapes.py

--- tryShapes.py
+++ tryShapes.py
@@ -1,3 +1,24 @@
 from shapeAndSize import *
 triangle.plot(3,4,20)
 anyShape.plot(4,6)
+
+# markers are: 
+# 'o': Circle (default)
+# 's': Square
+# '^': Triangle (pointing upwards)
+# 'v': Triangle (pointing downwards)
+# < (less than sign): Triangle (pointing left)
+# `>' (greater than sign): Triangle (pointing right)
+# 'p': Pentagram
+# 'x': X
+# '+': Plus sign
+# '|': Vertical line
+# '-': Horizontal line
+# '.': Point
+# 'D': Diamond
+# 'd': Thin diamond
+# 'h' (lowercase h): Hexagon
+# 'H' (uppercase H): Thick hexagon
+# 'p' (lowercase p): Pentagon
+# 'P' (uppercase P): Thick pentagon
+# '#': Hash
